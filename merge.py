3e3fd586470af48215879dfe93c2acfd417395fb
ilmfan <dasturchi.uzbek@gmail.com>
2021-08-26 11:10:52 +0500

    a

--- merge.py
+++ merge.py
@@ -1,31 +1,36 @@
-def merge(a, lb, mid, ub):
-    n1 = mid - lb + 1
-    n2 = ub - mid
-    l, r =[], []
-    for w in range(n1 + 1):
-        l.append(w)
-    for t in range(n2 + 1):
-        r.append(t)
-    for i in range(n1):
-        l[i] = a[lb + i]
-    for j in range(n2):
-        r[j] = a[mid + j]
-    l[n1 + 1] = "stop l"
-    r[n2 + 1] = "stop r"
-    i = 1
-    j = 1
+def merge(a, b):
+    sorted_list = []
+    len_a = len(a)
+    len_b = len(b)
+    i = 0
+    j = 0
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            sorted_list.append(a[i])
+            i += 1
+        else:
+            sorted_list.append(b[j])
+            j += 1
+    while i < len_a:
+        sorted_list.append(a[i])
+        i += 1
+    while j < len_b:
+        sorted_list.append(b[j])
+        j += 1
+    return sorted_list
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    l = arr[:mid]
+    r = arr[mid:]
+
+    l = merge_sort(l)
+    r = merge_sort(r)
+
+    return merge(l, r)
 
 
 
-
-
-def merge_sort(a, lb, ub):  # lb-lower bound and ub-upper bound
-    if lb < ub:
-        mid = (lb + ub) // 2
-        merge_sort(a, lb, mid)
-        merge_sort(a, (mid + 1), ub)
-        merge(a, lb, mid, ub)
-
-
-# list1 = [15, 5, 24, 8, 1, 3, 16, 10, 20]
-# print(merge_sort(list1, 0, 8))
